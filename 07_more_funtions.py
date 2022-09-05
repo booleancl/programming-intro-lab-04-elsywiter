1cf302cf98812b4153989deb222bfd8a0510efae
elsy <witerelsy@gmail.com>
2022-09-05 12:03:22 -0300

    funtions

---
+++ 07_more_funtions.py
@@ -0,0 +1,19 @@
+# Podemos crear o difinir nuestras propias funciones
+# Lo hacemos con la palabra especial "def" y el cuerpo
+# La función debe ir correctamente identado
+
+def chuchuwa(): (inst)
+    print("chuchuwa chuchuwa wa wa!")
+    print("chuchuwa chuchuwa wa wa!")
+    print("Atencion!, compañia")
+    print(inst)
+
+print(chuchuwa) 
+print(type(chuchuwa))
+
+# Llamado de la funcion
+
+chuchuwa("mano hacia el frente")
+# Las funciones deben llamarse o ejecutarse con los mismos parametros 
+que se asigno
+chuchuwa ("lengua afuera")
